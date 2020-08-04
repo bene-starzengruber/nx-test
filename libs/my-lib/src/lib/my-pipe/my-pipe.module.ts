aa8f85b1d8b21ebfedaad88a161f9cfa05de2218
bst <bst@celum.com>
2020-08-04 13:42:13 +0200

    transfer various other changes

--- libs/my-lib/src/lib/my-pipe/my-pipe.module.ts
+++ libs/my-lib/src/lib/my-pipe/my-pipe.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { MyPipe } from './my-pipe';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+            imports: [CommonModule],
             declarations: [MyPipe],
             exports: [MyPipe]
           })
